ebbf411fb842ecf4edd2841bf7026d32b33b52ce
ThiagoCoutinh0 <thiagoalvesdq@gmail.com>
2023-10-30 15:43:02 -0300

    config

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = '';
+    $dbUsername = '';
+    $dbPassword = '';
+    $dbName = '';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso"
+    }
+
+?>
